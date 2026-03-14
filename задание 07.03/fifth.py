4208170c22a1339b49c86ff1c93dc783714e1180
polisorbik <vach0974@gmail.com>
2026-03-15 00:14:49 +0300

    Add files via upload

---
+++ задание 07.03/fifth.py
@@ -0,0 +1,3 @@
+multiple_of_three=lambda num:'кратное трем' if num % 3 == 0 else 'не кратное трем'
+print(multiple_of_three(33))
+print(multiple_of_three(34))
---
+++ задание 07.03/first.py
@@ -0,0 +1,3 @@
+contains_a=lambda my_input: 'a' in my_input
+print(contains_a('xmozerx'))
+print(contains_a('Ржавый'))
